c175d8f9a0fc47b2049684f33d3ac6ff839e7b5b
Alexander Jason <alexanderjason526@gmail.com>
2024-05-30 16:25:44 +0700

    feat: implement client

---
+++ client/client.go
@@ -0,0 +1,67 @@
+package client
+
+import (
+	"if3230-tubes-wreckitraft/raft"
+	"log"
+	"net/rpc"
+)
+
+type Client struct {
+	serverAddress string
+	rpcClient     *rpc.Client
+}
+
+func NewClient(address string) (*Client, error) {
+	client, err := rpc.Dial("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		serverAddress: address,
+		rpcClient:     client,
+	}, nil
+}
+
+func (c *Client) Execute(command, key, value string) string {
+	args := &raft.CommandArgs{Command: command, Key: key, Value: value}
+	var reply raft.CommandReply
+	err := c.rpcClient.Call("Server.Execute", args, &reply)
+	if err != nil {
+		log.Fatalf("Execute error: %v", err)
+	}
+	return reply.Result
+}
+
+func (c *Client) RequestLog() []string {
+	var args raft.LogArgs
+	var reply raft.LogReply
+	err := c.rpcClient.Call("Server.RequestLog", &args, &reply)
+	if err != nil {
+		log.Fatalf("RequestLog error: %v", err)
+	}
+	return reply.Log
+}
+
+func (c *Client) Ping() string {
+	return c.Execute("ping", "", "")
+}
+
+func (c *Client) Get(key string) string {
+	return c.Execute("get", key, "")
+}
+
+func (c *Client) Set(key, value string) string {
+	return c.Execute("set", key, value)
+}
+
+func (c *Client) Strln(key string) string {
+	return c.Execute("strln", key, "")
+}
+
+func (c *Client) Del(key string) string {
+	return c.Execute("del", key, "")
+}
+
+func (c *Client) Append(key, value string) string {
+	return c.Execute("append", key, value)
+}
