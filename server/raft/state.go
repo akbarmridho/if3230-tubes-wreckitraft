16a4acfe5a58841df3dea64b36858c6c58545edf
chaerla <chelgchen@gmail.com>
2024-06-02 14:32:50 +0700

    feat: add raft state

---
+++ server/raft/state.go
@@ -0,0 +1,70 @@
+package raft
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type raftState struct {
+	currentTerm    uint64 // cache of stable store
+	commitIndex    uint64
+	lastAppliedLog uint64
+	lastLogIndex   uint64 // cache of log store
+	lastLogTerm    uint64
+	routinesGroup  sync.WaitGroup
+	state          NodeType
+	lock           sync.Mutex
+}
+
+func (r *raftState) getState() NodeType {
+	stateAddr := (*uint32)(&r.state)
+	return NodeType(atomic.LoadUint32(stateAddr))
+}
+
+func (r *raftState) setState(s NodeType) {
+	stateAddr := (*uint32)(&r.state)
+	atomic.StoreUint32(stateAddr, uint32(s))
+}
+
+func (r *raftState) getCurrentTerm() uint64 {
+	return atomic.LoadUint64(&r.currentTerm)
+}
+
+func (r *raftState) setCurrentTerm(term uint64) {
+	atomic.StoreUint64(&r.currentTerm, term)
+}
+
+func (r *raftState) getLastLog() (index, term uint64) {
+	r.lock.Lock()
+	index = r.lastLogIndex
+	term = r.lastLogTerm
+	r.lock.Unlock()
+	return
+}
+
+func (r *raftState) setLastLog(index, term uint64) {
+	r.lock.Lock()
+	r.lastLogIndex = index
+	r.lastLogTerm = term
+	r.lock.Unlock()
+}
+
+func (r *raftState) getLastIndex() uint64 {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	return r.lastLogIndex
+}
+
+func (r *raftState) getLastEntry() (uint64, uint64) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	return r.lastLogIndex, r.lastLogTerm
+}
+
+func (r *raftState) goFunc(f func()) {
+	r.routinesGroup.Add(1)
+	go func() {
+		defer r.routinesGroup.Done()
+		f()
+	}()
+}
