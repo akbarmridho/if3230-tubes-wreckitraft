6bf65c043ceae235117bee1a944f7dd94c41c1c3
chaerla <chelgchen@gmail.com>
2024-06-02 18:05:52 +0700

    chore: renamed key last voted cand

--- server/raft/stable.go
+++ server/raft/stable.go
@@ -3,12 +3,12 @@ package raft
 type StableKey string
 
 const (
-	keyCurrentTerm  StableKey = "CurrentTerm"
-	keyLastVoteTerm StableKey = "LastVoteTerm"
-	keyLastVoteCand StableKey = "LastVoteCand"
+	keyCurrentTerm   StableKey = "CurrentTerm"
+	keyLastVoteTerm  StableKey = "LastVoteTerm"
+	keyLastVotedCand StableKey = "LastVotedCand"
 )
 
 type StableStore interface {
 	Set(key StableKey, val uint64) error
-	Get(key StableKey) (uint64, error)
+	Get(key StableKey) (*uint64, error)
 }
