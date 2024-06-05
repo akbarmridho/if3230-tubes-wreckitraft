13b75552d2f4e58c87d18a3999d1d76adcb1e47e
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-06-05 19:03:49 +0700

    feat: send append entries

--- server/raft/leader.go
+++ server/raft/leader.go
@@ -1,1 +1,24 @@
 package raft
+
+import (
+	"fmt"
+	"if3230-tubes-wreckitraft/shared/logger"
+)
+
+func (r *RaftNode) sendAppendEntries(req ReceiveAppendEntriesArgs, resp *ReceiveAppendEntriesResponse, peer NodeConfiguration) {
+	logger.Log.Info("Sending append entries to: ", peer.ID)
+	err := peer.getRpcClient()
+
+	if err != nil {
+		logger.Log.Warn(err)
+		return
+	}
+
+	err = peer.rpcClient.Call("RaftNode.ReceiveAppendEntries", &req, resp)
+
+	if err != nil {
+		logger.Log.Warn(fmt.Sprintf("Send append entries to: %s failed", peer.ID))
+		return
+	}
+	logger.Log.Info("Append entries response: ", *resp)
+}
